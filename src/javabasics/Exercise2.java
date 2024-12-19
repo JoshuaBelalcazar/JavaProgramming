f4a89b108283adb6c52ba344c3dde13e7b309d26
JoshuaBelalcazar <belalcazarjoshua@gmail.com>
2024-12-18 20:27:26 -0500

    Problem 1 & 2 update

--- src/javabasics/Exercise2.java
+++ src/javabasics/Exercise2.java
@@ -1,4 +1,10 @@
 package javabasics;
 
 public class Exercise2 {
+    public static void main(String[] args) {
+        // Calculate the sum of 24 and 26
+        int sum = 24 + 26;
+        // Print the result of the addition
+        System.out.println(sum);
+    }
 }
